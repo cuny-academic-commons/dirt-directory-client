98c55fb447ad17869b53afa8e00f9cfb65ad83f2
Boone B Gorges <boonebgorges@gmail.com>
2014-08-19 15:34:03 -0400

    Improve PHPUnit bootstrap to work with BuddyPress

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,19 +1,27 @@
 <?php
 
-$_develop_dir = getenv( 'WP_DEVELOP_DIR' );
-if ( ! empty( $_develop_dir ) ) {
-	$_tests_dir = $_develop_dir . '/tests/phpunit/';
-} else {
-	$_tests_dir = getenv('WP_TESTS_DIR');
-	if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+if ( ! defined( 'BP_TESTS_DIR' ) ) {
+	define( 'BP_TESTS_DIR', dirname( __FILE__ ) . '/../../buddypress/tests/phpunit' );
 }
 
-require_once $_tests_dir . '/includes/functions.php';
+if ( file_exists( BP_TESTS_DIR . '/bootstrap.php' ) ) {
 
-function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../dirt-directory-client.php';
+	$_develop_dir = getenv( 'WP_DEVELOP_DIR' );
+	if ( ! empty( $_develop_dir ) ) {
+		$_tests_dir = $_develop_dir . '/tests/phpunit/';
+	} else {
+		$_tests_dir = getenv('WP_TESTS_DIR');
+		if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+	}
+
+	require_once $_tests_dir . '/includes/functions.php';
+
+	function _manually_load_plugin() {
+		require BP_TESTS_DIR . '/includes/loader.php';
+		require dirname( __FILE__ ) . '/../loader.php';
+	}
+	tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+	require $_tests_dir . '/includes/bootstrap.php';
 }
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
-require $_tests_dir . '/includes/bootstrap.php';
 
